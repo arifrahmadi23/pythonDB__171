deb357499be01dd735ca45aa3d3049af59c609c2
arifrahmadi23 <127088748+arifrahmadi23@users.noreply.github.com>
2023-11-26 09:49:25 +0700

    Delete latihanIfElse.py

--- latihanIfElse.py
+++
@@ -1,8 +0,0 @@
-def if_else_example():
-    age = 15
-    if (age>=18):
-        print ("Elegible for voting")
-    else:
-        print("not eligible for voting")
-        print ("statement after if statement")
-if_else_example()
